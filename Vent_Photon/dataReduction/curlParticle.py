8b4c5b0d05ef7ebddd686a1973f2ad7f0336275d
Dave G <davegutz@users.noreply.github.com>
2021-11-08 15:27:54 -0500

    Update curlParticle.py

--- Vent_Photon/dataReduction/curlParticle.py
+++ Vent_Photon/dataReduction/curlParticle.py
@@ -5,9 +5,11 @@
 # That's wehre the link to STREAM_URL came from.
 # Running:
 # 	install python:  3.6.8 is latest that supports curl.   Pick option to add python to path.
-# 	install dependencies:  cygwin> py -m pip install pycurl certifi
+# 	install dependencies:  cygwin> python -m pip install pycurl certifi
+#   go to particle.io and get token:https://console.particle.io/devices/
+#           click on device.   Select the little square icon.   Copy token.  Paste into STREAM_URL below
 #	running:   	cygwin> cd /cygdrive/c/Users/Dave/Documents/GitHub/myVentilator/Vent_Photon/dataReduction
-#				cygwin> py curlParticle.py
+#				cygwin> python curlParticle.py
 # Plotting:
 #	install sciLab 6.1 or later.  (Didn't work with 5.5.2)
 # 	install pdftk (use win8 compatibility)
@@ -16,6 +18,7 @@
 # MIT License
 #
 # 2021-Feb-02 	Dave Gutz 		Create
+# 2021-Nov-08   Dave Gutz       Verify operation after computer rebuild.   Token had changed
 
 import sys
 import pycurl
@@ -28,7 +31,7 @@ except ImportError:
     from io import StringIO, BytesIO
 import json
 
-STREAM_URL = "https://api.particle.io/v1/devices/events?access_token=7cab32b80bbbfdf8e1e83220c7479343a53e826e"
+STREAM_URL = "https://api.particle.io/v1/devices/events?access_token=9661ef494e1719ef619ff2c1e92742e40ddfac0d"
 DEVICE_NAME = 'vent'
 cts = datetime.datetime.now().isoformat()
 cts = cts.replace(':', '-')
@@ -50,6 +53,7 @@ class Client:
         # self.conn.setopt(pycurl.USERPWD, "%s:%s" % (USER, PASS))
         self.conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
         self.conn.setopt(self.conn.CAINFO, certifi.where())
+        # self.conn.setopt(self.conn.VERBOSE, True)
         self.conn.perform()
 
     def on_receive(self, byte_obj):
